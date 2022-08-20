e310e1fb3c27856184d6a0efb4e203efb8c68a64
Вахитов Рустам <therealrustam@yandex.ru>
2022-08-20 13:38:26 +0500

    fin

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
-from fps_inspector_sdk import fps_inspector
-import matplotlib
-import matplotlib.pyplot as plt
-import time
+import sys
 from os import startfile
 from time import sleep
-import os
 
-import numpy
 import pyautogui
-from psutil import process_iter
 import pydirectinput
-
-matplotlib.use('Agg')
-
-
-def main():
-    def float_formatter(x): return "%.5f" % x
-    numpy.set_printoptions(
-        formatter={'float_kind': float_formatter}, threshold=numpy.inf)
-    pid = os.getpid()
-    fps_inspector.start_fliprate_recording(pid)
-    time.sleep(10)
-    fps_inspector.stop_fliprate_recording()
-    data = fps_inspector.get_all_fliprates()
-    print(data)
-    plt.figure()
-    data[data.ScreenTime != 0][['FPS', 'FlipRate', 'ScreenTime']].plot(
-        x='ScreenTime', subplots=True)
-    plt.savefig('plot.png')
-    data.to_csv('scores.csv')
-    plt.close()
+from psutil import process_iter
 
 
-if __name__ == "__main__":
-    startfile('pno0001.exe')
-    sleep(15)
+if __name__ == '__main__':
+    kkrieger = sys.argv[1]
+    address = sys.argv[3]
+    startfile(kkrieger)
+    sleep(14)
+    screen1 = pyautogui.screenshot()
+    screen1.save(address+'screen1.png')
     pydirectinput.press('f11')
-    screen = pyautogui.screenshot('screenshot.png')
-    sleep(30)
     pydirectinput.press('enter', presses=2)
     pydirectinput.keyDown('w')
+    sleep(12)
     pydirectinput.press('f11')
-    sleep(20)
-    screen = pyautogui.screenshot('screenshot1.png')
+    screen2 = pyautogui.screenshot()
+    sleep(1)
     for proc in process_iter():
         if proc.name() == 'pno0001.exe':
             proc.kill()
+    screen2.save(address+'screen2.png')
